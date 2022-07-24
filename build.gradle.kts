57a17c0f070c690cbb9444dff399d52d071600a9
TozyMC <tozymc@gmail.com>
2022-07-24 08:14:38 +0700

    new: Gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    alias(libs.plugins.indra)
+    alias(libs.plugins.indra.checkstyle)
+    alias(libs.plugins.indra.publishing)
+    alias(libs.plugins.indra.publishing.sonatype)
+}
+
+val projectVersion: String by rootProject
+val projectDescription: String by rootProject
+
+group = "com.github.tozymc"
+version = projectVersion
+description = projectDescription
+
+indra {
+    javaVersions {
+        minimumToolchain(17)
+        target(17)
+        testWith(17)
+    }
+
+    checkstyle(libs.versions.checkstyle.get())
+
+    github("TozyMC", "plugin-loader") {
+        ci(true)
+    }
+    mitLicense()
+
+    configurePublications {
+        pom {
+            developers {
+                developer {
+                    id.set("TozyMC")
+                    name.set("Nguyễn Thanh Tân")
+                    email.set("tozymc@gmail.com")
+                }
+            }
+        }
+    }
+}
+
+dependencies {
+    compileOnlyApi(libs.jetbrains.annotations)
+
+    api(libs.slf4j)
+}
